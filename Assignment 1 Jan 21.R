11c9cf4bccba606f918fa2161ecd162848f2ed0f
Megan <ridgwaym@mcmaster.ca>
2021-01-28 20:21:36 -0500

    Made changes based on Ben's feedback

--- Assignment 1 Jan 21.R
+++ Assignment 1 Jan 21.R
@@ -3,23 +3,22 @@ getwd()
 
 #my chosen dataset
 guppy <- read.csv(file = "guppy.csv", head= TRUE)
-guppy
+summary(guppy)
 
 #create objects for each column
-attach(guppy)
 
 #check the sample size for each treatment
-table(treatment)
+table(guppy$treatment)
 
 #mean max parasite number out of the 10 days observed
-tapply(max.parasite.count, fish.Id, mean)
+with(guppy, tapply(max.parasite.count, fish.Id, mean))
 
 #mean max parasite number for each treatment, omitting all missing data
-tapply(max.parasite.count, treatment, mean, na.rm=TRUE)
+with (guppy,tapply(max.parasite.count, treatment, mean, na.rm=TRUE))
 
 #standard deviation max parasite number for each treatment, omitting all missing data
-tapply(max.parasite.count, treatment, sd, na.rm=TRUE)
+with(guppy,tapply(max.parasite.count, treatment, sd, na.rm=TRUE))
 
 #test for statistical difference of max parasite number between treatments
-guppy.aov <- aov(max.parasite.count ~ treatment)
+guppy.aov <- aov(max.parasite.count ~ treatment, data=guppy)
 summary(guppy.aov)
