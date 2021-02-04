b2c4be0455c017b3f99cdb66191bb0eca70ed7d1
Megan <ridgwaym@mcmaster.ca>
2021-02-04 17:36:13 -0500

    New code for assignment 3

--- README.md
+++ README.md
@@ -7,3 +7,12 @@ The data includes the fish ID, fish length, treatment/noise level the fish were 
 Assignment 2:
 
 With this data I would like to investigate how stress level impacts the immune response of fishes by examining changes in parasite load over time. I would also like to examine how a potential change in immune response impacts their longevity. In  order to sort my data into replicable components I would like to take the mean parasite load for each sampling day (days 1 - 10) within each treatment and plot each treatment as its own series on a line graph, demonstrating the change in immune response over time during or following a noise stressor to show a time course of the response. 
+
+
+Assignment 3:
+
+First, I wanted to look at the time course of the parasite count following the exposure to different types of noise stressors. I plotted each treatment individually first with the sampling day on the x axis, since each individual was observed over multiple days, and parasite count as the primary response variable on the y-axis. Since I am looking at how a variable changes over time I chose to display the relationship using a line graph. In addition to looking at each treatment individually, I chose to plot all three treatments together as a line graph in order to show how each treatment compares to each other in terms of their impact on parasite count. For aesthetics, I chose to display each treatment with a different coloured line. Next, I added up the parasite counts for all sampling days to get the total number of parasites that each individual had over the course of the sampling period. I then took the mean of all individuals for each treatment and plotted them on a bar graph. With this plot, I wanted to show the full extent of the parasite load for individuals in each treatment. For aesthetics I chose to use a simple theme so the data stood out and was easy to interpret. 
+
+
+
+
--- qmee_weekly_assignments.R
+++ qmee_weekly_assignments.R
@@ -42,3 +42,111 @@ plot.default(max.parasite.count, standard.length,
      ylab="Maximum Parasite Count", xlab="Standard Length")
 
 
+
+
+##New code for assignment 3
+
+library(tidyverse)
+
+view(guppy)
+
+library(dplyr)
+
+#pairing down columns to only the ones I need
+pdat <- select(guppy, treatment, day, parasite.count)
+
+
+#re-arranging the data by treatment and day
+timedat <- (pdat
+                %>% group_by(treatment, day)
+                %>% summarise_at("parasite.count",
+                                 list(mean=mean, sd=sd), na.rm=TRUE))
+print(timedat)
+
+
+#looking at each treatment independently - chronic noise first
+pdat_chr <- filter(pdat, treatment == "chronic noise")
+
+#re-arranging data by day
+timedat_chr <- (pdat_chr
+            %>% group_by(day)
+            %>% summarise_at("parasite.count",
+                             list(mean=mean, sd=sd), na.rm=TRUE))
+print(timedat_chr)
+
+
+#looking at acute noise only
+pdat_a <- filter(pdat, treatment == "acute noise")
+
+#sorting by day of sampling
+timedat_a <- (pdat_a
+                %>% group_by(day)
+                %>% summarise_at("parasite.count",
+                                 list(mean=mean, sd=sd), na.rm=TRUE))
+print(timedat_a)
+
+
+#looking at control group only
+pdat_no <- filter(pdat, treatment == "No Noise")
+
+#sorting by say of sampling
+timedat_no <- (pdat_no
+              %>% group_by(day)
+              %>% summarise_at("parasite.count",
+                               list(mean=mean, sd=sd), na.rm=TRUE))
+print(timedat_no)
+
+
+
+library(ggplot2)
+theme_set(theme_bw()) 
+
+#plotting each treatment separately
+#chronic
+pchr <- ggplot(timedat_chr,aes(day, mean))+labs(y = "Parasite count", x="Sampling day")
+print(pchr+geom_line())
+print(pchr+geom_line() +geom_point())
+
+#acute
+pa <- ggplot(timedat_a,aes(day, mean))+labs(y = "Parasite count", x="Sampling day")
+print(pa+geom_line())
+print(pa+geom_line() +geom_point())
+
+#control
+pno <- ggplot(timedat_no,aes(day, mean))+labs(y = "Parasite count", x="Sampling day")
+print(pno+geom_line())
+print(pno+geom_line() +geom_point())
+
+#plotting all 3 treatments together (line)
+pt <- ggplot(timedat,aes(day, mean, colour=treatment))+labs(y = "Parasite count", x="Sampling day")
+print(pt+geom_line())
+print(pt+geom_line() +geom_point())
+
+
+#taking the total parasite count over all days and comparing treatments 
+totdat <- select(guppy, fish.Id, treatment, parasite.count)
+
+
+#took the sum of the number of parasites over all sampling days for each individual
+totdatgrp <- (totdat
+            %>% group_by(fish.Id, treatment)
+            %>% summarise_at("parasite.count",
+                             list(sum=sum), na.rm=TRUE))
+
+#took the mean total parasite for each treatment
+totdatgrp2 <- (totdatgrp
+               %>% group_by(treatment)
+               %>% summarise_at("sum",
+                                list(mean=mean, sd=sd), na.rm=TRUE))
+
+
+#plotting the total parasite count over all sampling days across treatments (boxplot)
+btot <- ggplot(totdatgrp2,aes(treatment, mean))+
+  labs(y ="Mean total parasite count", x="Treatment")
+
+print(btot+geom_bar(stat = "identity", width = 0.5, fill="steelblue"))+
+  theme_minimal()
+
+
+
+
